082a3e73e8de3b880177d515013ee8b51ba8b5b0
notscuffed <47509036+notscuffed@users.noreply.github.com>
2020-01-10 22:40:28 +0100

    Json info generator now also saves spritesheet sequence info

--- RePKG.Application/Texture/TexJsonInfoGenerator.cs
+++ RePKG.Application/Texture/TexJsonInfoGenerator.cs
@@ -1,5 +1,6 @@
 using System;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RePKG.Core.Texture;
 
 namespace RePKG.Application.Texture
@@ -9,17 +10,36 @@ namespace RePKG.Application.Texture
         public string GenerateInfo(Tex tex)
         {
             if (tex == null) throw new ArgumentNullException(nameof(tex));
-            
-            return JsonConvert.SerializeObject(new
+
+            var json = new JObject
             {
-                bleedtransparentcolors = true,
-                clampuvs = tex.HasFlag(TexFlags.ClampUVs),
-                format = tex.Header.Format.ToString().ToLower(),
-                nomip = (tex.FirstImage.Mipmaps.Count == 1).ToString().ToLower(),
-                nointerpolation = tex.HasFlag(TexFlags.NoInterpolation).ToString().ToLower(),
-                nonpoweroftwo = (!NumberIsPowerOfTwo(tex.Header.ImageWidth) ||
-                                 !NumberIsPowerOfTwo(tex.Header.ImageHeight)).ToString().ToLower()
-            });
+                ["bleedtransparentcolors"] = true,
+                ["clampuvs"] = tex.HasFlag(TexFlags.ClampUVs),
+                ["format"] = tex.Header.Format.ToString().ToLower(),
+                ["nomip"] = (tex.FirstImage.Mipmaps.Count == 1).ToString().ToLower(),
+                ["nointerpolation"] = tex.HasFlag(TexFlags.NoInterpolation).ToString().ToLower(),
+                ["nonpoweroftwo"] = (!NumberIsPowerOfTwo(tex.Header.ImageWidth) ||
+                                     !NumberIsPowerOfTwo(tex.Header.ImageHeight)).ToString().ToLower()
+            };
+
+            if (tex.IsGif)
+            {
+                if (tex.FrameInfoContainer == null)
+                    throw new InvalidOperationException("TEX is animated but doesn't have frame info container");
+                
+                json["spritesheetsequences"] = new JArray
+                {
+                    new JObject
+                    {
+                        ["duration"] = 1, // not sure what this value is used for
+                        ["frames"] = tex.FrameInfoContainer.Frames.Count,
+                        ["width"] = tex.FrameInfoContainer.GifWidth,
+                        ["height"] = tex.FrameInfoContainer.GifHeight
+                    }
+                };
+            }
+
+            return JsonConvert.SerializeObject(json, Formatting.Indented);
         }
 
         private static bool NumberIsPowerOfTwo(int n)

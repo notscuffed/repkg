7620543faf3ee979ac6b98ca2e839d4ceaf9f847
notscuffed <47509036+notscuffed@users.noreply.github.com>
2020-06-01 13:02:04 +0200

    Increased maximum mipmap size, and increased version

--- RePKG.Application/Constants.cs
+++ RePKG.Application/Constants.cs
@@ -5,6 +5,6 @@ namespace RePKG.Application
         public const int MaximumFrameCount = 100_000;
         public const int MaximumImageCount = 100;
         public const int MaximumMipmapCount = 32;
-        public const int MaximumMipmapByteCount = 50_000_000; // 50 MB
+        public const int MaximumMipmapByteCount = 250_000_000; // 250 MB
     }
 }
